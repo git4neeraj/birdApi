97355cabe6f69602a6a9829a60bd104f8815d6be
Neeraj <neeraj.yadav@miles.in>
2015-09-18 02:14:04 +0530

    Incomplete tests but could have been done better

--- src/test/java/TestBirdApi.java
+++ src/test/java/TestBirdApi.java
@@ -1,7 +1,10 @@
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+import com.saltside.codingexcercise.birdApi.domain.Bird;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Date;
 
 import static com.jayway.restassured.RestAssured.expect;
 
@@ -26,4 +29,25 @@ public class TestBirdApi {
         expect().statusCode(200).contentType(ContentType.JSON).when()
                 .get("/birds/55f73676005d21e7adee3313");
     }
+
+    @Test
+    public void testDeleteBirdsWithId(){
+        expect().statusCode(200).contentType(ContentType.JSON).when()
+                .delete("/birds/55f73676005d21e7adee3313");
+    }
+
+    @Test
+    public void testPostBird(){
+        Bird bird = new Bird();
+        bird.setName("Tweety");
+        bird.setVisible(true);
+        bird.setAdded(new Date().toString());
+        bird.getContinents().add("Asia");
+        bird.getContinents().add("Latin");
+        bird.getContinents().add("North America");
+        bird.setFamily("f223");
+        expect().statusCode(200).contentType(ContentType.JSON).when()
+                .post("/birds",bird);
+    }
+
 }
